c6892f5029474f8627636d7537d56131078aa402
PiyushXCoder <piyush.raj.kit@gmail.com>
2024-07-23 22:44:23 +0530

    feat: add blog page

--- app/[blog]/page.tsx
+++ app/[blog]/page.tsx
@@ -1,11 +1,8 @@
-// import { useTranslations } from "next-intl";
-import { Posts } from "./posts";
-
 export default function Home() {
   return (
     <main className="w-full flex flex-col items-center">
       <div className="max-w-[50rem] w-full">
-        <Posts />
+        {/* TODO: Add Markdown to html */}
       </div>
     </main>
   );
