8a403798f9e349184c2a4d050a5d54388e35dff6
PiyushXCoder <piyush.raj.kit@gmail.com>
2024-08-03 11:37:06 +0530

    ref: use custom sitemap.xml route
    Use custom sitemap.xml route because `app/sitemap.ts` dosen't work well
    for dynamic sitemaps.

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  console.log(process.env.DATABASE_URL, process.env.BASE_URL);
+  if (
+    typeof process.env.DATABASE_URL == "undefined" ||
+    typeof process.env.BASE_URL == "undefined"
+  )
+    return NextResponse.json({ error: "Internal Error" }, { status: 500 });
+
+  const prisma = new PrismaClient();
+
+  const posts = await prisma.post.findMany();
+
+  let xml =
+    '<?xml version="1.0" encoding="UTF-8"?>' +
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+  posts.forEach((post) => {
+    xml +=
+      "<url>" +
+      "<loc>" +
+      process.env.BASE_URL +
+      "/" +
+      post.id +
+      "</loc>" +
+      "<lastmod>" +
+      post.published_at.toISOString() +
+      "</lastmod>" +
+      "<changefreq>weekly</changefreq>" +
+      "</url>";
+  });
+
+  xml += "</urlset>";
+
+  const headers = new Headers();
+  headers.set("Content-Type", "text/xml");
+  let response = new Response(xml, { headers });
+
+  return response;
+}
