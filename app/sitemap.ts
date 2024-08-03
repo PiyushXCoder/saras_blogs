99a276d0ebc052e9adb4cec89d046353289c07f7
PiyushXCoder <piyush.raj.kit@gmail.com>
2024-08-03 09:28:55 +0530

    fix(sitemap): DATABASE_URL not defined at build

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,19 +2,17 @@ import { PrismaClient } from "@prisma/client";
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  if (!process.env.DATABASE_URL) return [];
+
   const prisma = new PrismaClient();
 
   const posts = await prisma.post.findMany({});
 
-  const pageMaps: MetadataRoute.Sitemap = [];
-
-  posts.forEach((post) => {
-    pageMaps.push({
+  return posts.map((post) => {
+    return {
       url: process.env.BASE_URL + "/" + post.id,
       lastModified: post.published_at,
       changeFrequency: "weekly",
-    });
+    };
   });
-
-  return pageMaps;
 }
